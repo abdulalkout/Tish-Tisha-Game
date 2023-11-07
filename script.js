eb5afb21d0e4b61ebb8948c141759664026dbb13
abdulalkout <abdul.alkout@gmail.com>
2023-11-06 22:34:41 -0600

    Trying to implement other player

--- script.js
+++ script.js
@@ -1,133 +1,154 @@
-class Player {
-  constructor(name, score) {
-    this.name = name;
-    this.score = score;
-    this.rocks = 3;
-  }
+// class Player {
+//   constructor(name, score) {
+//     this.name = name;
+//     this.score = score;
+//     this.rocks = 3;
+//   }
 
-  decreaseRocks() {
-    this.rocks--;
-  }
-}
-
-class Computer {
-  constructor(name, score) {
-    this.name = name;
-    this.score = score;
-    this.rocks = 3;
-  }
-
-  PlaceTheRocks() {
-    let numberOfBox = Math.floor(Math.random() * 9) + 1;
-    console.log(numberOfBox);
-    const rockImg = document.querySelector(".compRockImg");
-    // const rockImgSelector = document.querySelector("")
-    // rockImgSelector.appendChild(rockImg);
-  }
-
-  decreaseRocks() {
-    this.rocks--;
-  }
-}
-
-const plyer1 = new Player("abdul");
-const plyer2 = new Player("Plyer2");
-const comp = new Computer("AI-Player");
-let rockNumber = 1;
-let round = 1;
-let BoxId = 0;
-//------------------------------DOM---------------------------------//
-//--Queries --//
-const tishTishaBoxs = document.querySelectorAll(".tishTishaBox");
-const player1RocksArr = document.querySelectorAll(".plyer1Rocks");
-
-const player1Button = document.querySelector("#placeTheRocks");
-
-const box1 = document.getElementById("1");
-const box2 = document.getElementById("2");
-const box3 = document.getElementById("3");
-const box4 = document.getElementById("4");
-const box5 = document.getElementById("5");
-const box6 = document.getElementById("6");
-const box7 = document.getElementById("7");
-const box8 = document.getElementById("8");
-const box9 = document.getElementById("9");
-
-const boxsArr = [
-  document.getElementById("1"),
-  document.getElementById("2"),
-  document.getElementById("3"),
-  document.getElementById("4"),
-  document.getElementById("5"),
-  document.getElementById("6"),
-  document.getElementById("7"),
-  document.getElementById("8"),
-  document.getElementById("9"),
-];
-
-//--Functions--//
-// function selectRocksPlaces(classImg, boxPlaceContent) {
-//   tishTishaBoxs.forEach((box) => {
-//     console.log(box);
-//     if (box.getElementsByTagName("img").length > 0) {
-//       console.log("there is a rock");
-//       box.removeEventListener();
-//     } else {
-//       box.addEventListener("click", (evt) => {
-//         let boxID = `${evt.target.id}`;
-//         let rockImgSelector = document.getElementById(`${boxID}`);
-//         const rockImg = document.querySelector(classImg);
-//         rockImgSelector.appendChild(rockImg);
-//       });
-//     }
-//   });
+//   decreaseRocks() {
+//     this.rocks--;
+//   }
 // }
 
-function selectRockBox(box, imgClassName, boxNumber) {
-  box.addEventListener("click", (e) => {
-    let rockImg = document.querySelector(imgClassName);
-    if (tishTishaBoxs[boxNumber].getElementsByTagName("img").length > 0) {
-      console.log("there is an img");
-    } else {
-      tishTishaBoxs[boxNumber].appendChild(rockImg);
-    }
-  });
-}
+// class Computer {
+//   constructor(name, score) {
+//     this.name = name;
+//     this.score = score;
+//     this.rocks = 3;
+//   }
 
-function playerPlaces(imgClassName) {
-  selectRockBox(boxsArr[0], imgClassName, 0);
-  selectRockBox(boxsArr[1], imgClassName, 1);
-  selectRockBox(boxsArr[2], imgClassName, 2);
-  selectRockBox(boxsArr[3], imgClassName, 3);
-  selectRockBox(boxsArr[4], imgClassName, 4);
-  selectRockBox(boxsArr[5], imgClassName, 5);
-  selectRockBox(boxsArr[6], imgClassName, 6);
-  selectRockBox(boxsArr[7], imgClassName, 7);
-  selectRockBox(boxsArr[8], imgClassName, 8);
-}
+//   PlaceTheRocks() {
+//     let numberOfBox = Math.floor(Math.random() * 9) + 1;
+//     console.log(numberOfBox);
+//     const rockImg = document.querySelector(".compRockImg");
+//     // const rockImgSelector = document.querySelector("")
+//     // rockImgSelector.appendChild(rockImg);
+//   }
 
-function playerButton(imgClassName) {
-  if (plyer1.rocks > 0) {
-    playerPlaces(imgClassName);
-    plyer1.decreaseRocks();
-    console.log(plyer1.rocks);
-  } else {
-    // player1Button.style.display = "none";
-  }
-}
-//--Event Listeners--//
-//--Event Listener for all Boxs--//
-
-console.log(tishTishaBoxs);
-
-// for (let i = 0; i <= boxsArr.length; i++) {
-//   selectRockBox(boxsArr[i], ".rockImg", i);
+//   decreaseRocks() {
+//     this.rocks--;
+//   }
 // }
 
-// -- Round Loop--//
+// const plyer1 = new Player("abdul");
+// const plyer2 = new Player("Plyer2");
+// const comp = new Computer("AI-Player");
+// let rockNumber = 1;
+// let round = 1;
+// let BoxId = 0;
+// //------------------------------DOM---------------------------------//
+// //--Queries --//
+// const tishTishaBoxs = document.querySelectorAll(".tishTishaBox");
+// const player1RocksArr = document.querySelectorAll(".plyer1Rocks");
+
+// const player1Button = document.querySelector("#placeTheRocks");
+// // const nextButton = document.querySelector("#nextPlayer");
+
+// const box1 = document.getElementById("1");
+// const box2 = document.getElementById("2");
+// const box3 = document.getElementById("3");
+// const box4 = document.getElementById("4");
+// const box5 = document.getElementById("5");
+// const box6 = document.getElementById("6");
+// const box7 = document.getElementById("7");
+// const box8 = document.getElementById("8");
+// const box9 = document.getElementById("9");
+
+// const boxsArr = [
+//   document.getElementById("1"),
+//   document.getElementById("2"),
+//   document.getElementById("3"),
+//   document.getElementById("4"),
+//   document.getElementById("5"),
+//   document.getElementById("6"),
+//   document.getElementById("7"),
+//   document.getElementById("8"),
+//   document.getElementById("9"),
+// ];
+
+// //--Functions--//
+// // function selectRocksPlaces(classImg, boxPlaceContent) {
+// //   tishTishaBoxs.forEach((box) => {
+// //     console.log(box);
+// //     if (box.getElementsByTagName("img").length > 0) {
+// //       console.log("there is a rock");
+// //       box.removeEventListener();
+// //     } else {
+// //       box.addEventListener("click", (evt) => {
+// //         let boxID = `${evt.target.id}`;
+// //         let rockImgSelector = document.getElementById(`${boxID}`);
+// //         const rockImg = document.querySelector(classImg);
+// //         rockImgSelector.appendChild(rockImg);
+// //       });
+// //     }
+// //   });
+// // }
+
+// function selectRockBox(box, imgClassName, boxNumber) {
+//   box.addEventListener(
+//     "click",
+//     (eventFunction = (e) => {
+//       let rockImg = document.querySelector(imgClassName);
+//       if (tishTishaBoxs[boxNumber].getElementsByTagName("img").length > 0) {
+//         console.log("there is an img");
+//       } else {
+//         tishTishaBoxs[boxNumber].appendChild(rockImg);
+//       }
+//       //   box.removeEventListener("click", eventFunction, false);
+//     })
+//   );
+// }
+
+// function playerPlaces(imgClassName) {
+//   selectRockBox(boxsArr[0], imgClassName, 0);
+//   selectRockBox(boxsArr[1], imgClassName, 1);
+//   selectRockBox(boxsArr[2], imgClassName, 2);
+//   selectRockBox(boxsArr[3], imgClassName, 3);
+//   selectRockBox(boxsArr[4], imgClassName, 4);
+//   selectRockBox(boxsArr[5], imgClassName, 5);
+//   selectRockBox(boxsArr[6], imgClassName, 6);
+//   selectRockBox(boxsArr[7], imgClassName, 7);
+//   selectRockBox(boxsArr[8], imgClassName, 8);
+// }
+
+// function playerButton(imgClassName) {
+//   if (plyer1.rocks > 0) {
+//     playerPlaces(imgClassName);
+//     plyer1.decreaseRocks();
+//     console.log(plyer1.rocks);
+//   } else {
+//     // player1Button.style.display = "none";
+//   }
+// }
+
+// //--Event Listeners--//
+// //--Event Listener for all Boxs--//
+
+// player1Button.addEventListener("click", (evt) => {
+//   if (plyer1.rocks > 0) {
+//     playerPlaces(".rockImg");
+//     plyer1.decreaseRocks();
+//     console.log(plyer1.rocks);
+//   } else {
+//     // player1Button.style.display = "none";
+//   }
+// });
+
+// // nextButton.addEventListener("click", (evt) => {
+// //   player1Button.removeEventListener("click", eventFunction, false);
+// // });
+
+// console.log(tishTishaBoxs);
+
+// // for (let i = 0; i <= boxsArr.length; i++) {
+// //   selectRockBox(boxsArr[i], ".rockImg", i);
+// // }
+
+// // -- Round Loop--//
 // while (round < 4) {
-//   //   selectRocksPlaces(".rockImg");
-//   //   selectRocksPlaces(".compRockImg");
+//   let classB = ".rockImg";
+//   playerPlaces(".rockImg");
+//   //   playerPlaces(".compRockImg");
 //   break;
 //   round++;
 // }
---
+++ script2.js
@@ -0,0 +1,73 @@
+class Player {
+  constructor(name, score) {
+    this.name = name;
+    this.score = score;
+    this.rocks = 3;
+  }
+
+  decreaseRocks() {
+    this.rocks--;
+  }
+}
+
+class Computer {
+  constructor(name, score) {
+    this.name = name;
+    this.score = score;
+    this.rocks = 3;
+  }
+
+  PlaceTheRocks() {
+    let numberOfBox = Math.floor(Math.random() * 9) + 1;
+    console.log(numberOfBox);
+    const rockImg = document.querySelector(".compRockImg");
+    // const rockImgSelector = document.querySelector("")
+    // rockImgSelector.appendChild(rockImg);
+  }
+
+  decreaseRocks() {
+    this.rocks--;
+  }
+}
+
+const plyer1 = new Player("abdul");
+const plyer2 = new Player("Plyer2");
+const comp = new Computer("AI-Player");
+let rockNumber = 1;
+let round = 1;
+
+//------------------------------DOM---------------------------------//
+//--Queries --//
+const tishTishaBoxs = document.querySelectorAll(".tishTishaBox");
+const player1RocksArr = document.querySelectorAll(".plyer1Rocks");
+
+let boxs = Array.from(document.getElementsByClassName("tishTishaBox"));
+const player1Img = "X";
+const player2Img = "O";
+let currentPlayer = player1Img;
+
+let boxSpaces = Array(9).fill(null);
+
+//--Functions--//
+
+const startGame = () => {
+  boxSpaces
+    .filter((box) => box !== null) // Filter out null values
+    .forEach((box) => box.addEventListener("click", boxClicked));
+};
+
+function boxClicked(e) {
+  const id = e.target.id;
+
+  if (!boxSpaces[id]) {
+    boxSpaces[id] = currentPlayer;
+    e.target.textContext = currentPlayer;
+
+    currentPlayer = currentPlayer == player1Img ? player2Img : player1Img;
+  }
+}
+
+startGame();
+
+//--Event Listeners--//
+//--Event Listener for all Boxs--//
